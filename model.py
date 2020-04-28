0a44064def3b4b2ffb51ce2ef072e208d75f82f2
lianqiann <al2374@cornell.edu>
2020-04-28 12:00:22 -0400

    upload

--- model.py
+++ model.py
@@ -1,8 +1,30 @@
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 nclasses = 43  # GTSRB as 43 classes
+
+
+class BaselineNet(nn.Module):
+    def __init__(self, gray=False):
+        super(BaselineNet, self).__init__()
+        input_chan = 1 if gray else 3
+        self.conv1 = nn.Conv2d(input_chan, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 43)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
 
 
 class TrafficSignNet(nn.Module):
@@ -69,3 +91,6 @@ class Stn(nn.Module):
         x = F.grid_sample(x, grid)
 
         return x
+
+
+
