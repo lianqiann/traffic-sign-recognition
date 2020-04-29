e248f3d4a48039c70be729f686ff005f96c7a109
lianqiann <al2374@cornell.edu>
2020-04-28 22:49:55 -0400

    upload my version

--- model.py
+++ model.py
@@ -13,14 +13,15 @@ class BaselineNet(nn.Module):
         self.conv1 = nn.Conv2d(input_chan, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc1 = nn.Linear(16 * 29* 29, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 43)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
+        
+        x = x.view(-1,16*29*29)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
